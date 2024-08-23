e53d213db4f4a396f475af850f642f9ac695a37f
Bogdan1017 <79454543+Bogdan1017@users.noreply.github.com>
2024-08-23 10:16:46 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,11 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static String msg = ""Hello qa.guru!!!!!!!"";
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello qa guru!!!!!!!");
+        System.out.println(msg);
 
 
     }
